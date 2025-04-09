c6c5dd6bb4e5023a4217ad51aa7ffa05ed49f89e
Mariana314 <marianaocampomedina@gmail.com>
2025-04-08 20:12:02 -0500

    Empezando a unir

---
+++ Principal/Principal.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Principal
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            btnRegistrarse = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Navy;
+            label1.Location = new Point(59, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(224, 34);
+            label1.TabIndex = 0;
+            label1.Text = "Rodando Store";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(68, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(188, 18);
+            label2.TabIndex = 1;
+            label2.Text = "Compra aqui tus ciclasGo";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(154, 125);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(543, 313);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // btnRegistrarse
+            // 
+            btnRegistrarse.BackColor = Color.Navy;
+            btnRegistrarse.Font = new Font("Arial", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnRegistrarse.ForeColor = SystemColors.Control;
+            btnRegistrarse.Location = new Point(674, 27);
+            btnRegistrarse.Name = "btnRegistrarse";
+            btnRegistrarse.Size = new Size(99, 32);
+            btnRegistrarse.TabIndex = 3;
+            btnRegistrarse.Text = "Registrarse";
+            btnRegistrarse.UseVisualStyleBackColor = false;
+            btnRegistrarse.Click += btnRegistrarse_Click;
+            // 
+            // Principal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnRegistrarse);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Principal";
+            Text = "Principal";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Button btnRegistrarse;
+    }
+}
